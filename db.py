5a15af91f6202e406fb7462eb9986679172f1c40
akshaycolombus2 <akshaycolombus2@gmail.com>
2024-10-30 08:22:06 -0300

    db.py added which will make the official connection

---
+++ db.py
@@ -0,0 +1,13 @@
+import psycopg2
+from config import config
+
+def connect():
+    conn = None
+    try:
+        params =config()
+        conn = psycopg2.connect(**params)
+        print("Connected to the Database Succesfully")
+        return conn
+    
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
